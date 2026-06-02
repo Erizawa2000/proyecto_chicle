75edc59a2154a422ecf619dacc72bf7778a660cb
Eric Ramos Aragón <eric.ramos@countigo.org>
2026-06-02 13:40:02 -0400

    Reajuste de fecha de reencuentro

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ APP_TITLE = "Nuestro Reencuentro"
 PARTNER_1 = "Eri"
 PARTNER_2 = "Nai"
 
-REUNION_DATE = datetime(2026, 7, 1, 18, 0, 0)
+REUNION_DATE = datetime(2026, 7, 26, 11, 28, 0)
 
 MAIN_MESSAGE = "Cada día que pasa es un día menos para volver a verte."
